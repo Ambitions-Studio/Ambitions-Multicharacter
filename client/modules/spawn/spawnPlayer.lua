56390048476d83d6278971d6463bf3881a8ec921
Primordial-Devv <primordial.devv@gmail.com>
2025-08-21 18:43:00 +0200

    FEAT - Starting spawn flow

---
+++ client/modules/spawn/spawnPlayer.lua
@@ -0,0 +1,35 @@
+local spawnConfig = require('config.spawn')
+
+--- Prepare the character for the selection screen
+local function PrepareCharacter()
+  local spawnCoords = spawnConfig.characterSelectionSpawn
+  local spawnModel = "mp_m_freemode_01"
+
+  RequestModel(pedModel)
+  while not HasModelLoaded(pedModel) do
+      Wait(500)
+  end
+
+  SetPlayerModel(playerId(), spawnModel)
+  local playerPed = PlayerPedId()
+
+
+  SetEntityCoords(playerPed, spawnCoords.x,spawnCoords.y, spawnCoords.z, true, false, false, false)
+  SetEntityHeading(playerPed, spawnCoords.w)
+
+  SetPlayerControl(playerId(), false, 0)
+
+  ShutdownLoadingScreen()
+  ShutdownLoadingScreenNui()
+
+  DoScreenFadeIn(500)
+end
+
+CreateThread(function()
+  while not NetworkIsPlayerActive(PlayerId()) do
+    Wait(100)
+  end
+
+  DoScreenFadeOut(0)
+  PrepareCharacter()
+end)
---
+++ config/spawn.lua
@@ -0,0 +1,6 @@
+return {
+  characterSlots = 2,
+  playerCanDeleteCharacter = true,
+  characterSelectionSpawn = vector4(0.0, 0.0, 70.0, 0.0),
+  playerSpawn = vector4(0.0, 0.0, 70.0, 0.0),
+}
