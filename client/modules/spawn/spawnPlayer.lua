583d5fc2a07f0d56704d95eed1e7f83b97bfe0c4
Primordial-Devv <primordial.devv@gmail.com>
2025-08-23 11:05:06 +0200

    FIX - Removing invisibility of the player

--- client/modules/spawn/spawnPlayer.lua
+++ client/modules/spawn/spawnPlayer.lua
@@ -1,5 +1,17 @@
 local spawnConfig = require('config.spawn')
 
+--- Set the default clothes for the player
+---@param ped number The ped of the player
+local function SetDefaultClothes(ped)
+  for i = 0, 11 do
+      SetPedComponentVariation(ped, i, 0, 0, 4)
+  end
+
+  for i = 0, 7 do
+      ClearPedProp(ped, i)
+  end
+end
+
 --- Prepare the character for the selection screen
 local function PrepareCharacter()
   local spawnCoords = spawnConfig.characterSelectionSpawn
@@ -12,6 +24,7 @@ local function PrepareCharacter()
 
   SetPlayerModel(playerId(), spawnModel)
   local playerPed = PlayerPedId()
+  SetDefaultClothes(playerPed)
 
 
   SetEntityCoords(playerPed, spawnCoords.x,spawnCoords.y, spawnCoords.z, true, false, false, false)
