42394277677c4ba13113684ce0824b42ef57e07b
Primordial-Devv <primordial.devv@gmail.com>
2025-08-23 12:36:09 +0200

    FIX - Fixing camera animation

--- client/modules/spawn/spawnPlayer.lua
+++ client/modules/spawn/spawnPlayer.lua
@@ -16,31 +16,32 @@ end
 ---@param playerPed number The player ped
 ---@param spawnCoords vector4 The spawn coordinates
 local function CreateCinematicCamera(playerPed, spawnCoords)
-  local startX = spawnCoords.x - 5.0 * math.cos(math.rad(spawnCoords.w))
-  local startY = spawnCoords.y - 5.0 * math.sin(math.rad(spawnCoords.w))
-  local startZ = spawnCoords.z + 3.0
+  local headingRad = math.rad(spawnCoords.w)
+  local cosHeading = math.cos(headingRad)
+  local sinHeading = math.sin(headingRad)
 
-  local endX = spawnCoords.x + 2.0 * math.cos(math.rad(spawnCoords.w))
-  local endY = spawnCoords.y + 2.0 * math.sin(math.rad(spawnCoords.w))
-  local endZ = spawnCoords.z + 0.5
+  local startX = spawnCoords.x - (8.0 * cosHeading)
+  local startY = spawnCoords.y - (8.0 * sinHeading)
+  local startZ = spawnCoords.z + 4.0
 
-  local startCam = CreateCamWithParams("DEFAULT_SCRIPTED_CAMERA", startX, startY, startZ, 0.0, 0.0, spawnCoords.w, 50.0, false, 0)
-  local endCam = CreateCamWithParams("DEFAULT_SCRIPTED_CAMERA", endX, endY, endZ, -5.0, 0.0, spawnCoords.w + 180.0, 50.0, false, 0)
+  local endX = spawnCoords.x + (2.5 * cosHeading)
+  local endY = spawnCoords.y + (2.5 * sinHeading)
+  local endZ = spawnCoords.z + 0.3
 
-  PointCamAtEntity(startCam, playerPed, 0.0, 0.0, 0.0, true)
-  PointCamAtEntity(endCam, playerPed, 0.0, 0.0, 0.0, true)
+  local startCam = CreateCamWithParams("DEFAULT_SCRIPTED_CAMERA", startX, startY, startZ, 0.0, 0.0, spawnCoords.w, 45.0, false, 0)
+  local endCam = CreateCamWithParams("DEFAULT_SCRIPTED_CAMERA", endX, endY, endZ, -2.0, 0.0, spawnCoords.w + 180.0, 45.0, false, 0)
+
+  PointCamAtEntity(startCam, playerPed, 0.0, 0.0, 0.3, true)
+  PointCamAtEntity(endCam, playerPed, 0.0, 0.0, 0.3, true)
 
   SetCamActive(startCam, true)
   RenderScriptCams(true, false, 0, true, true)
 
-  SetCamActiveWithInterp(endCam, startCam, 3000, 1, 1)
+  SetCamActiveWithInterp(endCam, startCam, 4000, 1, 1)
 
   CreateThread(function()
-    Wait(3500)
+    Wait(4500)
     DestroyCam(startCam, false)
-    SetCamActive(endCam, false)
-    RenderScriptCams(false, true, 1000, true, true)
-    DestroyCam(endCam, false)
   end)
 end
 
