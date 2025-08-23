4778c9289a998fe70e0a0ff5766d42d5886c4609
Primordial-Devv <primordial.devv@gmail.com>
2025-08-23 12:01:45 +0200

    FEAT - Ading correct character selection coords

--- config/spawn.lua
+++ config/spawn.lua
@@ -1,6 +1,6 @@
 return {
   characterSlots = 2,
   playerCanDeleteCharacter = true,
-  characterSelectionSpawn = vector4(0.0, 0.0, 70.0, 0.0),
+  characterSelectionSpawn = vector4(925.45, 11.70, 113.55, 301.35),
   playerSpawn = vector4(0.0, 0.0, 70.0, 0.0),
 }
