c9854cdf4396a205b819a95871f5a8d733e17b24
Primordial-Devv <primordial.devv@gmail.com>
2025-08-23 12:24:23 +0200

    FEAT - First try with cinematic camera

--- client/modules/spawn/spawnPlayer.lua
+++ client/modules/spawn/spawnPlayer.lua
@@ -12,6 +12,38 @@ local function SetDefaultClothes(ped)
   end
 end
 
+--- Create cinematic camera effect for character selection
+---@param playerPed number The player ped
+---@param spawnCoords vector4 The spawn coordinates
+local function CreateCinematicCamera(playerPed, spawnCoords)
+  local startX = spawnCoords.x - 5.0 * math.cos(math.rad(spawnCoords.w))
+  local startY = spawnCoords.y - 5.0 * math.sin(math.rad(spawnCoords.w))
+  local startZ = spawnCoords.z + 3.0
+
+  local endX = spawnCoords.x + 2.0 * math.cos(math.rad(spawnCoords.w))
+  local endY = spawnCoords.y + 2.0 * math.sin(math.rad(spawnCoords.w))
+  local endZ = spawnCoords.z + 0.5
+
+  local startCam = CreateCamWithParams("DEFAULT_SCRIPTED_CAMERA", startX, startY, startZ, 0.0, 0.0, spawnCoords.w, 50.0, false, 0)
+  local endCam = CreateCamWithParams("DEFAULT_SCRIPTED_CAMERA", endX, endY, endZ, -5.0, 0.0, spawnCoords.w + 180.0, 50.0, false, 0)
+
+  PointCamAtEntity(startCam, playerPed, 0.0, 0.0, 0.0, true)
+  PointCamAtEntity(endCam, playerPed, 0.0, 0.0, 0.0, true)
+
+  SetCamActive(startCam, true)
+  RenderScriptCams(true, false, 0, true, true)
+
+  SetCamActiveWithInterp(endCam, startCam, 3000, 1, 1)
+
+  CreateThread(function()
+    Wait(3500)
+    DestroyCam(startCam, false)
+    SetCamActive(endCam, false)
+    RenderScriptCams(false, true, 1000, true, true)
+    DestroyCam(endCam, false)
+  end)
+end
+
 --- Prepare the character for the selection screen
 local function PrepareCharacter()
   local spawnCoords = spawnConfig.characterSelectionSpawn
@@ -26,7 +58,6 @@ local function PrepareCharacter()
   local playerPed = PlayerPedId()
   SetDefaultClothes(playerPed)
 
-
   SetEntityCoords(playerPed, spawnCoords.x,spawnCoords.y, spawnCoords.z, true, false, false, false)
   SetEntityHeading(playerPed, spawnCoords.w)
 
@@ -36,6 +67,10 @@ local function PrepareCharacter()
   ShutdownLoadingScreenNui()
 
   DoScreenFadeIn(500)
+
+  -- Start cinematic camera effect after fade in
+  Wait(500)
+  CreateCinematicCamera(playerPed, spawnCoords)
 end
 
 CreateThread(function()
